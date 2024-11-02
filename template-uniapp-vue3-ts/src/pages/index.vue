875272532f2b4484f500e5be9b0e94e85ae310c0
zaeyee <zaeyee@163.com>
2024-11-02 12:44:48 +0800

    chore(uniapp-vue3-ts): update deps.

--- template-uniapp-vue3-ts/src/pages/index.vue
+++ template-uniapp-vue3-ts/src/pages/index.vue
@@ -4,7 +4,7 @@ const title = ref('Hello')
 
 <template>
   <view class="flex flex-col justify-center items-center">
-    <image class="mx-auto mb-50rpx mt-200rpx h-200rpx w-200rpx" src="/static/images/logo.png"></image>
+    <image class="mx-auto mb-6 mt-25 h-25 w-25" src="/static/images/logo.png" />
     <view class="flex justify-center">
       <text class="text-lg c-info">{{ title }}</text>
     </view>
