f81f6c833ed107fcf8cd68d246635233727793c8
zaeyee <zaeyee@163.com>
2025-10-29 17:11:15 +0800

    chore: v1.4.7

--- template-uniapp-ts/src/pages/index.vue
+++ template-uniapp-ts/src/pages/index.vue
@@ -6,7 +6,7 @@ const title = ref('Hello')
   <view class="flex flex-col items-center justify-center">
     <image class="mx-auto mb-6 mt-25 h-25 w-25" src="/static/images/logo.png" />
     <view class="flex justify-center">
-      <text class="text-lg c-info">{{ title }}</text>
+      <text class="text-lg c-link">{{ title }}</text>
     </view>
   </view>
 </template>
